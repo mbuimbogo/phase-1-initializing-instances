bc91c3ad7a93a147a5b2030da01d5970f59d9283
“mbuimbogo” <peter.mbogo@student.moringaschool.com>
2022-09-09 15:27:55 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your code here
+
+class Breakfast{
+    
+    constructor(food, drink){
+this.food = food;
+this.drink = drink;
+    }
+
+}
+const bfast = new Breakfast('eggs', 'juice') 
+bfast.food;
+bfast.drink;
+
+class Lunch {
+
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+    
+    this.drink = drink;
+    }
+}
+ const newlunch = new Lunch("side salad", "broccoli cheddar soup", "iced tea")
+ newlunch.soup;
+ newlunch.salad;
+ newlunch.drink;
+
+ class Dinner {
+    
+    #dessert
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+
+ }
+ const newdinner = new Dinner("balsamic salad", "consomme", "filet mignon", "cheesecake")
+ newdinner.salad;
+ newdinner.soup;
+ newdinner.entree;
+ newdinner.dessert;
